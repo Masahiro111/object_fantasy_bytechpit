be2b692a8951c2d6862c0349ac4ba895d9126ee1
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-04-21 18:00:13 +0900

    finished part 4-3

--- main.php
+++ main.php
@@ -11,6 +11,7 @@
 // require_once('./classes/WhiteMage.php');
 
 require_once('./lib/Loader.php');
+require_once('./lib/Utillity.php');
 
 // オートロード
 $loader = new Loader();
@@ -33,23 +34,6 @@ $turn = 1;
 $isFinishFlg = false;
 
 $messageObj = new Message;
-
-function isFinish($objects)
-{
-    $deathCnt = 0; // HPが0以下の仲間の数
-    foreach ($objects as $object) {
-        // 1人でもHPが０を超えていたらfalseを返す
-        if ($object->getHitPoint() > 0) {
-            return false;
-        }
-        $deathCnt++;
-    }
-
-    //仲間の買うzが死亡数（HPが0以下の数）と同じであればtrueを返す
-    if ($deathCnt === count($objects)) {
-        return true;
-    }
-}
 
 
 while (!$isFinishFlg) {
