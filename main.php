8a1df42cf0e807cf1682bf9bee51c0c1c773c8a9
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-04-22 00:41:02 +0900

    Revert "finished part 1-7"
    This reverts commit 3d937ac3829444ffaec32969795ffa8838f32afb.

--- main.php
+++ main.php
@@ -1,37 +1,4 @@
 <?php
 
-// echo "処理のはじまりはじまり～！\n\n";
+echo "処理のはじまりはじまり～！\n\n";
 
-require_once('./classes/Human.php');
-require_once('./classes/Enemy.php');
-
-$tiida = new Human();
-$goblin = new Enemy();
-
-$tiida->name = "ティーダ";
-$goblin->name = "ゴブリン";
-
-$turn = 1;
-while ($tiida->hitPoint > 0 && $goblin->hitPoint > 0) {
-
-    echo "*** $turn ターン目 ***\n\n";
-    // 現在のHPの表示
-    if ($turn === 1) {
-        echo "START ! \n";
-        $turn++;
-    } else {
-        echo "STATUS \n";
-    }
-    echo $tiida->name . " : " . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
-    echo $goblin->name . " : " . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n\n";
-
-    $tiida->doAttack($goblin);
-    echo "\n\n";
-
-    $goblin->doAttack($tiida);
-    echo "\n\n";
-}
-
-echo "★★★ 戦闘終了 ★★★\n";
-echo $tiida->name . "　：　" . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
-echo $goblin->name . "　：　" . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n\n";
