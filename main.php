47b77556d581445f1faefd83e647714987062da0
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-04-22 18:27:49 +0900

    finished part 2

--- main.php
+++ main.php
@@ -5,24 +5,22 @@ require_once('./classes/Human.php');
 require_once('./classes/Enemy.php');
 require_once('./classes/Brave.php');
 
-$tiida = new Brave();
-$goblin = new Enemy();
+$tiida = new Brave("ティーダ");
+$goblin = new Enemy("ゴブリン");
 
-$tiida->name = "ティーダ";
-$goblin->name = "ゴブリン";
 
 $turn = 1; // ここを追加
 
 //========== ここから追加する ==========
 // どちらかのHPが０になるまで繰り返す
-while ($tiida->hitPoint > 0 && $goblin->hitPoint > 0) {
+while ($tiida->getHitPoint() > 0 && $goblin->getHitPoint() > 0) {
     //========== ここまで追加する ==========
 
     echo "*** $turn ターン目 ***\n\n"; // ここを追加
 
     // 現在のHPの表示
-    echo $tiida->name . "　：　" . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
-    echo $goblin->name . "　：　" . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n";
+    echo $tiida->getName() . "　：　" . $tiida->getHitPoint() . "/" . $tiida::MAX_HITPOINT . "\n";
+    echo $goblin->getName() . "　：　" . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n";
     echo "\n";
 
     // 攻撃
@@ -37,6 +35,6 @@ while ($tiida->hitPoint > 0 && $goblin->hitPoint > 0) {
 
 //========== ここから追加する ==========
 echo "★★★ 戦闘終了 ★★★\n\n";
-echo $tiida->name . "　：　" . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
-echo $goblin->name . "　：　" . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n\n";
+echo $tiida->getName() . "　：　" . $tiida->getHitPoint() . "/" . $tiida::MAX_HITPOINT . "\n";
+echo $goblin->getName() . "　：　" . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n\n";
 //========== ここまで追加する ==========
