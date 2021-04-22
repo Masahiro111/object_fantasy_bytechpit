49b2605b8146b5936726e136a1c23c5ae9fa06b3
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-04-22 17:22:21 +0900

    finished part 1-6

--- classes/Human.php
+++ classes/Human.php
@@ -2,25 +2,22 @@
 
 class Human
 {
-
     // プロパティ
-    const MAX_HITPOINT = 100; // 最大ＨＰの定義　定数
-    public $name; // 人間の名前
-    public $hitPoint = 100; // 現在のＨＰ
-    public $attackPoint = 20; // 攻撃力
+    const MAX_HITPOINT = 100;
+    public $name;
+    public $hitPoint = 100;
+    public $attackPoint = 20;
 
-    // メソッド
     public function doAttack($enemy)
     {
-        echo "[" . $this->name . "] の攻撃！\n";
-        echo "【" . $enemy->name . "】に" . $this->attackPoint
-            . $enemy->tookDamage($this->attackPoint);
+        echo "「" . $this->name . "」の攻撃！\n";
+        echo "「" . $enemy->name . "」に" . $this->attackPoint . "のダメージ！\n";
+        $enemy->tookDamage($this->attackPoint);
     }
 
     public function tookDamage($damage)
     {
         $this->hitPoint -= $damage;
-
         if ($this->hitPoint < 0) {
             $this->hitPoint = 0;
         }
