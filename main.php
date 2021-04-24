89959131b612619ec66f206add8062eeebb6f6d0
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-04-24 22:13:44 +0900

    finished part 3-5

--- main.php
+++ main.php
@@ -6,6 +6,7 @@ require_once('./classes/Enemy.php');
 require_once('./classes/Brave.php');
 require_once('./classes/BlackMage.php');
 require_once('./classes/WhiteMage.php');
+require_once('./classes/Message.php');
 
 $members = array();
 $members[] = new Brave('ティーダ');
@@ -19,8 +20,26 @@ $enemies[] = new Enemy('モルボル', 30);
 
 
 $turn = 1; // ここを追加
-
 $isFinishFlg = false;
+
+$messageObj = new Message();
+
+// 終了条件の判定
+function isFinish($objects)
+{
+    $deathCnt = 0;
+    foreach ($objects as $object) {
+        // 一人でもHPが０を超えていたらfalseを返す
+        if ($object->getHitPoint() > 0) {
+            return false;
+        }
+        $deathCnt++;
+    }
+
+    if ($deathCnt === count($objects)) {
+        return true;
+    }
+}
 
 //========== ここから追加する ==========
 // どちらかのHPが０になるまで繰り返す
@@ -29,72 +48,28 @@ while (!$isFinishFlg) {
 
     echo "*** $turn ターン目 ***\n\n"; // ここを追加
 
-    foreach ($members as $member) {
-        echo $member->getName() . " : " . $member->getHitPoint() . "/" . $member::MAX_HITPOINT . "\n";
-    }
-    echo "\n";
-    foreach ($enemies as $enemy) {
-        echo $enemy->getName() . " : " . $enemy->getHitPoint() . "/" . $enemy::MAX_HITPOINT . "\n";
-    }
-    echo "\n";
+    // 仲間の表示
+    $messageObj->displayStatusMessage($members);
 
-    foreach ($members as $member) {
-        // $enemyIndex = rand(0, count($enemies) - 1);
-        // $enemy = $enemies[$enemyIndex];
+    // 敵の表示
+    $messageObj->displayStatusMessage($enemies);
 
-        if (get_class($member) == "WhiteMage") {
-            $member->doAttackWhiteMage($enemies, $members);
-        } else {
-            $member->doAttack($enemies);
-        }
-        echo "\n";
-    }
-    echo "\n";
+    // 仲間の攻撃
+    $messageObj->displayAttackMessage($members, $enemies);
+    // 敵の攻撃
+    $messageObj->displayAttackMessage($enemies, $members);
 
-    // 現在のHPの表示
-    // echo $tiida->getName() . "　：　" . $tiida->getHitPoint() . "/" . $tiida::MAX_HITPOINT . "\n";
-    // echo $goblin->getName() . "　：　" . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n";
-    // echo "\n";
 
-    // 攻撃
-    // $tiida->doAttack($goblin);
-    // echo "\n";
-    // $goblin->doAttack($tiida);
-
-    foreach ($enemies as $enemy) {
-        // $memberIndex = rand(0, count($members) - 1);
-        // $member = $members[$memberIndex];
-        $enemy->doAttack($members);
-    }
-    echo "\n";
-
-    // 仲間の全滅チェック
-    $deathCnt = 0; // HPが0以下の仲間のかず
-    foreach ($members as $member) {
-        if ($member->getHitPoint() > 0) {
-            $isFinishFlg = false;
-            break;
-        }
-        $deathCnt++;
-    }
-    if ($deathCnt === count($members)) {
-        $isFinishFlg = true;
-        echo "GAME OVER ... \n\n";
+    // 戦闘終了条件のチェック 仲間全員のHPが0 または、敵全員のHPが0
+    $isFinishFlg = isFinish($members);
+    if ($isFinishFlg) {
+        $message = "GAME OVER ....\n\n";
         break;
     }
 
-    // 敵の全滅チェック
-    $deathCnt = 0; // HPが0以下の敵の数
-    foreach ($enemies as $enemy) {
-        if ($enemy->getHitPoint() > 0) {
-            $isFinishFlg = false;
-            break;
-        }
-        $deathCnt++;
-    }
-    if ($deathCnt === count($enemies)) {
-        $isFinishFlg = true;
-        echo "♪♪♪ファンファーレ♪♪♪\n\n";
+    $isFinishFlg = isFinish($enemies);
+    if ($isFinishFlg) {
+        $message = "♪♪♪ファンファーレ♪♪♪\n\n";
         break;
     }
 
@@ -105,14 +80,11 @@ while (!$isFinishFlg) {
 //========== ここから追加する ==========
 echo "★★★ 戦闘終了 ★★★\n\n";
 
-// 現在のHPの表示
-foreach ($members as $member) {
-    echo $member->getName() . " : " . $member->getHitPoint() . "/" . $member::MAX_HITPOINT . "\n";
-}
-echo "\n";
-foreach ($enemies as $enemy) {
-    echo $enemy->getName() . " : " . $enemy->getHitPoint() . "/" . $enemy::MAX_HITPOINT . "\n";
-}
+echo $message;
+
+$messageObj->displayStatusMessage($members);
+$messageObj->displayStatusMessage($enemies);
+
 echo "\n";
 
 // echo $tiida->getName() . "　：　" . $tiida->getHitPoint() . "/" . $tiida::MAX_HITPOINT . "\n";
